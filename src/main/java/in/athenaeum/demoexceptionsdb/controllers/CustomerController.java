b346180e16d7e6f988dae137f68d7d49d9cef6fa
Vivekanand P V <vivekanandpv@outlook.com>
2020-11-30 14:48:46 +0530

    Cors error resolution

--- src/main/java/in/athenaeum/demoexceptionsdb/controllers/CustomerController.java
+++ src/main/java/in/athenaeum/demoexceptionsdb/controllers/CustomerController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/customers")
+@CrossOrigin(origins = {"http://localhost:3000", "http://app.domain.com"})
 public class CustomerController {
     private final ICustomerService customerService;
 
